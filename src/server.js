4c3d4796f68ba4d06ed757c25cabcd2d59330eaf
Frederico T <frederico.thofehrn@acad.pucrs.br>
2020-08-07 20:13:13 -0300

    término da aula 4, backend e rotas, falta só banco SQL

--- src/server.js
+++ src/server.js
@@ -11,23 +11,77 @@ const proffys = [{
 }
 ]
 
+const subjects = [
+
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+
+]
+
+const weekdays = [
+
+    "Domingo",
+    "Segunda-Feira",
+    "Terça-Feira",
+    "Quarta-Feira",
+    "Quinta-Feira",
+    "Sexta-Feira",
+    "Sábado",
+
+
+]
+
+function getSubject(subjectNumber) {
+    const arrayPosition = +subjectNumber - 1
+    return subjects[arrayPosition]
+}
+
+
 function pageLanding(req, res) {
-    return res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 }
 
 
 function pageStudy(req, res) {
-    return res.sendFile(__dirname + "/views/study.html")
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 
 function pageGiveClasses(req, res) {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+    const data = req.query
+
+    const isNotEmpty = Object.keys(data).length > 0
+
+    if (isNotEmpty) {
+
+        data.subject = getSubject(data.subject)
+        proffys.push(data)    
+
+        return res.redirect("/study")
+    } 
+    
+    return res.render("give-classes.html", { subjects, weekdays})
 }
 
 const express = require('express')
 const server = express()
 const nunjucks = require("nunjucks")
+
+//configurar nunjucks
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
 
 server.use(express.static("public")) // <----
 //configurar arquivos estáticos (css, scripts, imagens)
