d17d9e3d94ed9bffa49a85dd0894825fe9497bd5
Frederico T <frederico.thofehrn@acad.pucrs.br>
2020-08-06 22:24:20 -0300

    aula 3 finalizada, revisar css/js pq n criou horário

---
+++ scripts/addField.js
@@ -0,0 +1,20 @@
+document.querySelector("#add-time")
+
+.addEventListener('click', cloneField)
+
+
+
+
+function cloneField() {
+    //duplica campo
+    const newtimefield = document.querySelector('.schedule-item').cloneNode(true)
+    //limpa os campos
+    const timefield = newtimefield.querySelectorAll('input')
+
+   timefield.forEach(function(timefield){
+    timefield.value = ""
+   })
+    document.querySelector('#schedule-items').appendChild(newtimefield)
+
+
+}
